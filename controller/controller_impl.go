a23319ea270da1f406d80d16a97c3646f9b5e63b
Andhika Danger <andhikadanger666@gmail.com>
2025-06-26 23:29:10 +0700

    ctrl: login

--- controller/controller_impl.go
+++ controller/controller_impl.go
@@ -28,3 +28,16 @@ func (ctrl *ControllerImpl) GetProducts(c *fiber.Ctx) error {
 
 	return web.SuccessResponse[[]*domain.Products](c, fiber.StatusOK, "OK", result)
 }
+
+func (ctrl *ControllerImpl) Login(c *fiber.Ctx) error {
+	var user domain.User
+	if err := c.BodyParser(&user); err != nil {
+		return web.ErrorResponse(c, fiber.StatusBadRequest, "Error", err.Error())
+	}
+	result, err := ctrl.svc.Login(c.Context(), user.Username, user.Password)
+	if err != nil {
+		return web.ErrorResponse(c, fiber.StatusBadRequest, "Error", err.Error())
+	}
+
+	return web.SuccessResponse[*domain.User](c, fiber.StatusOK, "OK", result)
+}
