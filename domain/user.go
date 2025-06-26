4f397a15ef811eb36064c26891ad13e43a453d9d
Andhika Danger <andhikadanger666@gmail.com>
2025-06-27 00:09:39 +0700

    domain: omitempty user

--- domain/user.go
+++ domain/user.go
@@ -3,9 +3,9 @@ package domain
 import "github.com/google/uuid"
 
 type User struct {
-	Id        uuid.UUID `json:"id"`
-	Username  string    `json:"username"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Password  string    `json:"password"`
+	Id        uuid.UUID `json:"id,omitempty"`
+	Username  string    `json:"username,omitempty"`
+	FirstName string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
+	Password  string    `json:"password,omitempty"`
 }
