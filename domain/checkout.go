f48affd8199c6601aeb1232846577178afd110fb
Andhika Danger <andhikadanger666@gmail.com>
2025-06-28 16:42:04 +0700

    domain: order struct

---
+++ domain/checkout.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Checkout struct {
+	Id          uuid.UUID  `json:"id"`
+	Nama        string     `json:"nama"`
+	Username    string     `json:"username"`
+	ProductId   string     `json:"product_id"`
+	ProductName string     `json:"product_name"`
+	Quantity    int        `json:"quantity"`
+	Total       float64    `json:"total"`
+	CreatedAt   *time.Time `json:"created_at"`
+	NoHp        string     `json:"no_hp"`
+	Alamat      string     `json:"alamat"`
+	Kecamatan   string     `json:"kecamatan"`
+	Desa        string     `json:"desa"`
+}
