a7324b18f2887b6c360e10313e11f1255bde091b
Andhika Danger <andhikadanger666@gmail.com>
2025-06-30 08:19:01 +0700

    svc: use error handler

--- service/service_impl.go
+++ service/service_impl.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"khaira-catering-user/domain"
+	"khaira-catering-user/helper"
 	"khaira-catering-user/repository"
 
 	"github.com/google/uuid"
@@ -25,34 +26,29 @@ func NewServiceImpl(db *sql.DB, repo repository.Repository) Service {
 func (svc *ServiceImpl) GetProducts(ctx context.Context) ([]*domain.Products, error) {
 	result, err := svc.repo.GetProducts(ctx, svc.db)
 	if err != nil {
-		return nil, err
+		return nil, helper.NewError(err.Error())
 	}
-
 	return result, nil
 }
 
 func (svc *ServiceImpl) Login(ctx context.Context, username string, password string) (*domain.User, error) {
 	result, err := svc.repo.Login(ctx, svc.db, username, password)
 	if err != nil {
-		return nil, err
+		return nil, helper.NewError(err.Error())
 	}
-
 	err = bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(password))
 	if err != nil {
-		return nil, err
+		return nil, helper.NewError("password tidak cocok")
 	}
-
 	return result, nil
-
 }
 
 func (svc *ServiceImpl) Register(ctx context.Context, entity *domain.User) (*domain.User, error) {
 	userId := uuid.New()
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(entity.Password), bcrypt.DefaultCost)
 	if err != nil {
-		return nil, err
+		return nil, helper.NewError("gagal mengenkripsi password")
 	}
-
 	user := &domain.User{
 		Id:        userId,
 		Username:  entity.Username,
@@ -60,57 +56,50 @@ func (svc *ServiceImpl) Register(ctx context.Context, entity *domain.User) (*dom
 		LastName:  entity.LastName,
 		Password:  string(hashedPassword),
 	}
-
 	result, err := svc.repo.Register(ctx, svc.db, user)
 	if err != nil {
-		return nil, err
+		return nil, helper.NewError(err.Error())
 	}
-
 	return result, nil
 }
 
 func (svc *ServiceImpl) AddToCart(ctx context.Context, username string, product *domain.Products, quantity int) error {
 	err := svc.repo.AddToCart(ctx, username, product, quantity, svc.db)
 	if err != nil {
-		return err
+		return helper.NewError(err.Error())
 	}
-
 	return nil
 }
 
 func (svc *ServiceImpl) GetCart(ctx context.Context, username string) ([]*domain.CartItem, error) {
 	cart, err := svc.repo.GetCart(ctx, username)
 	if err != nil {
-		return nil, err
+		return nil, helper.NewError(err.Error())
 	}
-
 	return cart, nil
 }
 
 func (svc *ServiceImpl) DeleteCartItem(ctx context.Context, username string, productID string) error {
 	err := svc.repo.DeleteCartItem(ctx, username, productID)
 	if err != nil {
-		return err
+		return helper.NewError(err.Error())
 	}
-
 	return nil
 }
 
 func (svc *ServiceImpl) DeleteCartItemByQuantity(ctx context.Context, username string, productId string, quantity int) error {
 	err := svc.repo.DeleteCartItemByQuantity(ctx, username, productId, quantity)
 	if err != nil {
-		return err
+		return helper.NewError(err.Error())
 	}
-
 	return nil
 }
 
 func (svc *ServiceImpl) CreateOrder(ctx context.Context, orderDetails *domain.Checkout) error {
 	tx, err := svc.db.Begin()
 	if err != nil {
-		return err
+		return helper.NewError("gagal memulai transaksi")
 	}
-
 	defer func() {
 		if err != nil {
 			tx.Rollback()
@@ -118,22 +107,18 @@ func (svc *ServiceImpl) CreateOrder(ctx context.Context, orderDetails *domain.Ch
 			tx.Commit()
 		}
 	}()
-
 	id := uuid.New()
-
 	err = svc.repo.CreateOrder(ctx, tx, orderDetails, id)
 	if err != nil {
-		return err
+		return helper.NewError(err.Error())
 	}
-
 	return nil
 }
 
 func (svc *ServiceImpl) GetOrderHistory(ctx context.Context, username string) ([]*domain.Checkout, error) {
 	res, err := svc.repo.GetOrderHistory(ctx, svc.db, username)
 	if err != nil {
-		return nil, err
+		return nil, helper.NewError(err.Error())
 	}
-
 	return res, nil
 }
